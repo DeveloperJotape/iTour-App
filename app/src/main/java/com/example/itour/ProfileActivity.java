28392deb4c972ee2af7d666884b3c21ecbe64795
João Pedro <developerjotape@gmail.com>
2024-10-07 09:58:26 -0300

    New: Integração com Firebase - Login e Cadastro
    Errors: Não abre as telas de Login e Cadastro

--- app/src/main/java/com/example/itour/ProfileActivity.java
+++ app/src/main/java/com/example/itour/ProfileActivity.java
@@ -1,13 +1,19 @@
 package com.example.itour;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     //Codificar
@@ -15,6 +21,29 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if(user == null) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+        /* ADICIONAR LOGOUT NA TELA DE PERFIL
+        *
+        *   button.setOnClickListner(new View.OnClickListner() {
+        *
+        *       FirebaseAuth.getInstance().signOut();
+        *
+        *      Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        *       startActivity(intent);
+        *       finish();
+        *
+        *   })
+        *
+        * */
 
     }
 }
--- app/src/main/java/com/example/itour/RegisterActivity.java
+++ app/src/main/java/com/example/itour/RegisterActivity.java
@@ -2,25 +2,103 @@ package com.example.itour;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextView txtToLogin;
+    TextInputEditText editTextName, editTextEmail, editTextPassword;
+    Button buttonRegister;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), /*Trocar após criar home*/ProfileActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextName = findViewById(R.id.edTxtName);
+        editTextEmail = findViewById(R.id.edTxtEmail);
+        editTextPassword = findViewById(R.id.edTxtPassword);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        progressBar = findViewById(R.id.progrssBar);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String name, email, password;
+                name = String.valueOf(editTextName.getText());
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(RegisterActivity.this, "Insira seu nome!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(RegisterActivity.this, "Insira seu email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "Insira sua senha!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Conta criada.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterActivity.this, "Falha na autenticação.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 
     public void screenLogin(View view) {
         Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(in);
+        finish();
     }
 }
